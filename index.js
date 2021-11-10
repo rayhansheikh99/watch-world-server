63e3ce3524684a6d2c9aa0f48aa5c769c26775ca
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2021-11-10 12:56:32 +0600

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express = require('express')
-const app = express()
-const port = 5000
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ouksj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected')
+        const database = client.db('watch_world');
+        const productCollection = database.collection('products');
+        // const orderCollection = database.collection('orders');
+        
+
+        //GET Products API
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+
+       
+
+    }
+
+        finally {
+            // await client.close();
+        }
+    }
+    run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Get Ready to Fight')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server is running`,port)
 })
